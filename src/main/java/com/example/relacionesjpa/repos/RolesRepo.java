4bf2f8a767376108be33172ac8e4969ad0ee7d30
borilio <exprimetuandroid@gmail.com>
2022-05-08 16:55:37 +0200

    Momento 1: Definidos Usuarios y Roles, y sus correspondientes repositorios.
    Si accedemos a /api/usuarios obtenemos los usuarios.
    Si accedemos a /api/roles obtenemos los roles.
    
    Todo por separado.

--- src/main/java/com/example/relacionesjpa/repos/RolesRepo.java
+++ src/main/java/com/example/relacionesjpa/repos/RolesRepo.java
@@ -4,7 +4,6 @@ import com.example.relacionesjpa.models.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 @RepositoryRestResource(path = "roles")
